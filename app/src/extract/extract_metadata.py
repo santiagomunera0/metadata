34a5ddd59313b43790be0e7f86061b2f7dca40fd
Santiago Munera <s.munera1902@gmail.com>
2023-09-17 17:26:32 -0500

    Add PDF Extract

--- app/src/extract/extract_metadata.py
+++ app/src/extract/extract_metadata.py
@@ -4,12 +4,15 @@ from src.common.common_tools import parameters
 import src.extract.power_point as ppt
 import src.extract.excel as xls
 import src.extract.word as doc
+import src.extract.pdf as pdf
 
 def extract_metadata():
     ppt_file = os.path.join(parameters['docs'], 'test.pptx')
     xls_file = os.path.join(parameters['docs'], 'test.xlsx')
-    doc_file = file = os.path.join(parameters['docs'], 'test.docx') 
+    doc_file = os.path.join(parameters['docs'], 'test.docx') 
+    pdf_file = os.path.join(parameters['docs'], 'test.pdf')
 
     ppt.extract_metadata(ppt_file)
     xls.extract_metadata(xls_file)
     doc.extract_metadata(doc_file)  
+    pdf.extract_metadata(pdf_file)
